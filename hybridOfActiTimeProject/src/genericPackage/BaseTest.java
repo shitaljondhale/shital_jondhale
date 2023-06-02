324fb96a456773a2984e580278f56298b73742c3
Suraj Surve <Suraj Surve@STSURVE>
2023-06-02 10:41:00 +0530

    commit

---
+++ hybridOfActiTimeProject/src/genericPackage/BaseTest.java
@@ -0,0 +1,82 @@
+package genericPackage;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.Duration;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest extends Flib implements IAutoconstant {
+	//its use to open the browsers and close the browser
+static WebDriver driver;
+	
+	// It is use to open the browser & close the browser
+	@BeforeMethod
+	public void setUp() throws IOException
+	{
+		Flib flib = new Flib();
+		
+		String browserValue = flib.readPropertyData(PROP_PATH,"browser");
+		String url = flib.readPropertyData(PROP_PATH,"url");
+		
+		if(browserValue.equalsIgnoreCase("chrome"))
+		{
+			System.setProperty(CHROME_KEY, CHROME_PATH);
+			driver=new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+			driver.get(url);
+		}
+		else if(browserValue.equalsIgnoreCase("firefox"))
+		{
+			System.setProperty(GECKO_KEY, GECKO_PATH);
+			driver=new FirefoxDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+			driver.get(url);
+		}
+		else if(browserValue.equalsIgnoreCase("edge"))
+		{
+			System.setProperty(EDGE_KEY, EDGE_PATH);
+			driver=new EdgeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+			driver.get(url);
+		}
+		else 
+		{
+			Reporter.log("InvalidBrowser Value!!!",true);
+		}
+	}
+
+	public void faliedMethodToTakeScreenshot(String failedMethod)
+	{
+		try
+		{
+		TakesScreenshot ts = (TakesScreenshot)driver;
+		File src = ts.getScreenshotAs(OutputType.FILE);
+		File dest = new File(SCREENSHOT_PATH+failedMethod+".png");
+		Files.copy(src,dest);
+		}
+		catch(Exception e)
+		{
+			
+		}
+	}
+	
+	@AfterMethod
+	public void tearDown()
+	{
+		driver.quit();
+	}
+	
+}
---
+++ hybridOfActiTimeProject/src/genericPackage/Flib.java
@@ -0,0 +1,73 @@
+package genericPackage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.sl.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import graphql.com.google.common.collect.Table.Cell;
+
+public class Flib {
+
+	//it is use to Store generic resuable method
+	
+	public String readExcelData(String excelPath,String sheetName,int rowCount,int cellCount) throws EncryptedDocumentException, IOException
+	{
+         FileInputStream fis = new FileInputStream(excelPath);	
+         Workbook wb = WorkbookFactory.create(fis);
+         org.apache.poi.ss.usermodel.Sheet sheet = wb.getSheet(sheetName);
+         Row row = sheet.getRow(rowCount);
+         Cell cell = row.getCell(cellCount);
+         String data = cell.getStringCellValue();
+         return data;
+	}
+	
+	// read property Data
+	
+		public String readPropertyData(String propPath,String key) throws IOException
+		{
+			FileInputStream fis = new FileInputStream(propPath);
+			Properties prop = new Properties();
+			prop.load(fis);
+			String data = prop.getProperty(key);
+			return data;
+		}
+		
+	// genralize the rowCount
+	
+	public int rowCount(String excelPath,String sheetName) throws EncryptedDocumentException, IOException
+	{
+		FileInputStream fis = new FileInputStream(excelPath);
+		Workbook wb = WorkbookFactory.create(fis);
+		org.apache.poi.ss.usermodel.Sheet sheet = wb.getSheet(sheetName);
+		int rc = sheet.getLastRowNum();
+		return rc;	
+	}
+	
+	//write Excel data
+	
+	public void writeExcelData(String excelPath,String sheetName,int rowCount,int cellCount,String data) throws EncryptedDocumentException, IOException
+	{
+
+		FileInputStream fis = new FileInputStream(excelPath);
+		Workbook wb = WorkbookFactory.create(fis);
+		org.apache.poi.ss.usermodel.Sheet sheet = wb.getSheet(sheetName);
+		Row row = sheet.getRow(rowCount);
+		Cell cell = row.createCell(cellCount);
+		cell.setCellValue(data);
+		
+		FileOutputStream fos = new FileOutputStream(excelPath);
+		wb.write(fos);
+	}
+	
+	
+	}
+}
+}
